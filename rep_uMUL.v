2d1d72dc6128c70bd64a26e17a280b82d5f70605
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-06-28 19:28:54 -0400

    Added real uMUL file

---
+++ rep_uMUL.v
@@ -0,0 +1,52 @@
+//By Alexander Peacock
+//email: alexpeacock56ten@gmail.com
+
+`include "sobolrng.v"
+
+module rep_uMUL #(
+    parameter BITWIDTH = 8
+) (
+    input wire iClk,
+    input wire iRstN,
+    input wire iA,
+    input wire [BITWIDTH - 1: 0] iB,
+    input wire loadB,
+    input wire iClr,
+    output reg oB, 
+    output reg oMult
+);
+
+    reg [BITWIDTH-1:0] iB_buff; //to store a value in block
+    wire [BITWIDTH-1:0] sobolseq;
+
+    always@(posedge iClk or negedge iRstN) begin
+        if(~iRstN) begin
+            iB_buff <= 0;
+        end else begin
+            if(loadB) begin
+                iB_buff <= iB;
+            end else begin
+                iB_buff <= iB_buff;
+            end
+            
+        end
+    end
+
+    sobolrng #(
+        .BITWIDTH(BITWIDTH)
+    ) u_sobolrng (
+        .iClk(iClk),
+        .iRstN(iRstN),
+        .iEn(iA), 
+        .iClr(iClr),
+        .sobolseq(sobolseq)
+    );
+
+    always@(*) begin
+        oB <= (iB_buff > sobolseq);
+        oMult <= iA & (iB_buff > sobolseq);
+    end
+
+
+
+endmodule
